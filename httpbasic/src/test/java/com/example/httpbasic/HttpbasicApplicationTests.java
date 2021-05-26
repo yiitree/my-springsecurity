c1ca71313f46e10e47ee835b1f42b0e5397b759a
zengrui <772701414@qq.com>
2021-05-26 13:39:13 +0800

    Httpbasic登录模式

---
+++ httpbasic/src/test/java/com/example/httpbasic/HttpbasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.httpbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpbasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
